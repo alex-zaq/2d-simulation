c9d4ecd5f871f72b3ef5491a540d82132160cde6
Alexey Melnikov <melnikov1991@bk.ru>
2025-02-18 18:31:28 +0300

    fix: modify __init__ for backend and frontend

---
+++ src/simulation/__init__.py
@@ -0,0 +1,5 @@
+from .backend import Backend, Config
+from .frontend import ConsoleFrontend, GuiFrontend
+from .simulation import Simulation
+
+__all__ = ["Simulation", "Backend", "Config", "ConsoleFrontend", "GuiFrontend"]
---
+++ src/simulation/backend/__init__.py
@@ -0,0 +1,3 @@
+from .backend import Backend, Config
+
+__all__ = ["Backend", "Config"]
