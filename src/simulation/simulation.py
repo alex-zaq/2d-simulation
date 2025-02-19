944c2a4509cd898bb0a94d990a519b1d5d0bc02b
alex <melnikov1991@bk.ru>
2025-02-19 23:18:38 +0300

    feat: add simulation methods

--- src/simulation/simulation.py
+++ src/simulation/simulation.py
@@ -1,12 +1,34 @@
  
  
-    
-    
 class Simulation:
     def __init__(self, backend, frontend):
         self.backend = backend
         self.frontend = frontend
         
-        
     def run(self):
-        ...
+        self.init()
+        while True:
+            self.next_step()
+            if self.frontend.get_stop_flag():
+                break
+        
+        
+    def init(self):
+        self.backend.init()
+        state = self.backend.get_state()
+        self.frontend.set_state(state)
+        
+        
+    def next_step(self):
+        self.backend.next_step()
+        state = self.backend.get_state()
+        self.frontend.set_state(state)
+        self.frontend.render()
+        
+    def previous_step(self):
+        self.backend.previous_step()
+        state = self.backend.get_state()
+        self.frontend.set_state(state)
+        self.frontend.render()
+        
+        
