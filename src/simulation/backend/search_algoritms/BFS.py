4cf15f44de40722c50cb21a9ebfc1a6d7f1503f8
Alexey Melnikov <melnikov1991@bk.ru>
2025-02-20 16:42:56 +0300

    feat: add basic BFS

--- src/simulation/backend/search_algoritms/BFS.py
+++ src/simulation/backend/search_algoritms/BFS.py
@@ -1,12 +1,29 @@
-from abc import ABC, abstractmethod
-
-from ..entities import Entity
+from collections import deque
 
 
-class Search(ABC):
-    @abstractmethod
-    def search(self, map: dict, entity: Entity, target: Entity): ...
+class BFS:
+    @classmethod
+    def search(cls, map, entity, target):
+        parents = {}
+        visited = set()
 
+        queue = deque([entity])
+        visited.add(entity)
 
-class BFS(Search):
-    def search(self, map: dict, entity: Entity, target: Entity) -> list[(int, int)]: ...
+        while queue:
+            current = queue.popleft()
+
+            if current == target:
+                path = []
+                while current in parents:
+                    current = parents[current]
+                    path.append(current)
+                return path[::-1]
+
+            for neighbor in current.get_neighbors(map):
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append(neighbor)
+                    parents[neighbor] = current
+
+        return None
