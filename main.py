e56a9892aa66758e1a2858859985c358484cc56c
Alexey Melnikov <melnikov1991@bk.ru>
2025-02-20 16:44:02 +0300

    feat: modify main

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 from src.simulation import Backend, Backend_config, Gui_config, GuiFrontend, Simulation
 
 if __name__ == "__main__":
-    
-    
     backend_config = Backend_config(
-        map_size= (100, 100),
+        map_size=(100, 100),
         creatures_count=10,
         predators_ratio=0.1,
         grass_count=30,
         rock_count=30,
-        search_algoritm="bfs",
-        breeding = False,
+        predator_search_algoritm="bfs",
+        herbivore_search_algoritm="bfs",
+        # herbivore_search_algoritm="A*",
+        predator_breeding=False,
+        herbivore_breeding=False,
+        herbivore_escaping_skill=False,
     )
-    
+
     gui_config = Gui_config(
         resolution=(800, 600),
-        rock_pict="images/rock.png",
-        predator_pict="images/predator.png",
-        herbivore_pict="images/herbivore.png",
-        grass_pict="images/grass.png",
+        rock_pict="images/Obstacle.png",
+        predator_pict="images/Predator.png",
+        herbivore_pict="images/Herbivore.png",
+        grass_pict="images/Grass.png",
     )
-            
-    
+
     app = Simulation(
-        backend = Backend(backend_config),
-        frontend = GuiFrontend(gui_config),
+        backend=Backend(backend_config),
+        frontend=GuiFrontend(gui_config),
     )
-    
+
     app.run()
