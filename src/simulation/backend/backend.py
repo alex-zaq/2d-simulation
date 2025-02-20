58e164f26b63d312d4b2dcac6938ddb95a9ffb77
Alexey Melnikov <melnikov1991@bk.ru>
2025-02-20 16:43:39 +0300

    feat: modify backend

--- src/simulation/backend/backend.py
+++ src/simulation/backend/backend.py
@@ -1,23 +1,58 @@
 from dataclasses import dataclass
 
+from .entities.predator import Herbivore, Predator
+from .search_algoritms import BFS
+
 
 @dataclass
 class BackendState:
-    ...
+    pass
+
 
 class Backend:
     def __init__(self, config):
         self.config = config
+        self.history = []
 
-    def init(self):
-        self.map_init()
-        self.spawn_entites()
+    def map_init(self):
+        self._set_search_stategy()
+        x_count, y_count = self.config.map_size
+        self.map = {
+            (x, y): self.spawner.spawn(x, y)
+            for x in range(x_count)
+            for y in range(y_count)
+        }
 
+    def set_spawner(self, spawner):
+        self.spawner = spawner
 
-    def map_init(self): ...
+    def _set_search_stategy(self):
+        self.set_search_algorithm(
+            Predator, self.get_search_algorithm(self.config.predator_search_strategy)
+        )
+        self.set_search_algorithm(
+            Herbivore, self.get_search_algorithm(self.config.predator_search_strategy)
+        )
 
-    def spawn_entites(self): ...
+    def get_search_algorithm(self, name):
+        match name:
+            case "BFS":
+                return BFS
+            case "A*":
+                raise NotImplementedError
 
-    def next_step(self): ...
-    
-    def previous_step(self): ...
+    def set_search_algorithm(self, entity, search_algoritm):
+        entity.search_algoritm = search_algoritm
+
+    def do_move(self):
+        entities = list(self.map.values())
+        for entity in entities:
+            entity.move(map)
+
+    def next_step(self):
+        self.history.append(self.map)
+        self.do_move()
+
+    def previous_step(self):
+        self.map = self.history.pop()
+        self.do_move()
