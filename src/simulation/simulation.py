ad84304e769ea2feed8cacd53a4a9430aadc2462
Alexey Melnikov <melnikov1991@bk.ru>
2025-02-24 17:25:59 +0300

    fix: modify simulation

--- src/simulation/simulation.py
+++ src/simulation/simulation.py
@@ -1,34 +1,13 @@
- 
- 
 class Simulation:
     def __init__(self, backend, frontend):
         self.backend = backend
         self.frontend = frontend
-        
-    def run(self):
-        self.init()
-        while True:
-            self.next_step()
-            if self.frontend.get_stop_flag():
-                break
-        
-        
-    def init(self):
+        self._simulation_init()
+
+    def _simulation_init(self):
         self.backend.map_init()
-        back_end_map = self.backend.get_map()
-        self.frontend.update_map(back_end_map)
-        
-        
-    def next_step(self):
-        self.backend.next_step()
-        state = self.backend.get_state()
-        self.frontend.set_state(state)
-        self.frontend.render()
-        
-    def previous_step(self):
-        self.backend.previous_step()
-        state = self.backend.get_state()
-        self.frontend.set_state(state)
-        self.frontend.render()
-        
-        
+        self.frontend.use_backend(self.backend)
+
+    def run(self):
+        self.frontend.window_init()
+        self.frontend.run()
