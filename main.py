a24a27ce4b1d1ca0c6fe24ebbd27398d84a55a67
alex <melnikov1991@bk.ru>
2025-02-25 09:14:45 +0300

    fix: change main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.simulation import Backend, Backend_config, Gui_config, GuiFrontend, Sim
 
 if __name__ == "__main__":
     backend_config = Backend_config(
-        map_size=(15, 15),
+        map_size=(25, 15),
         herbivores_start_count=1,
         predators_start_count=0,
         grass_count=0,
@@ -15,18 +15,18 @@ if __name__ == "__main__":
     )
 
     gui_config = Gui_config(
-        resolution=(800, 600),
         rock_pict="images/Obstacle.png",
         predator_pict="images/Predator.png",
         herbivore_pict="images/Herbivore.png",
         grass_pict="images/Grass.png",
+        grid_color = "gray",
     )
 
-    app = Simulation(
-        backend=Backend(backend_config),
-        frontend=GuiFrontend(gui_config),
-    )
+   
+    b = Backend(backend_config)
+    f = GuiFrontend(gui_config)
+    f.use_backend(b)
 
-    app.run()
-    
-    print("end")
+
+    f.window_init()
+    f.run()
