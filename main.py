0c83e12767de6d0d6ae0d6578e3f3e4153c31a24
Alexey Melnikov <melnikov1991@bk.ru>
2025-02-27 16:15:37 +0300

    fix: change main

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from src.simulation import Backend, Backend_config, Gui_config, GuiFrontend, Simulation
+from src.simulation import Backend, Backend_config, Gui_config, GuiFrontend
 
 if __name__ == "__main__":
     backend_config = Backend_config(
         map_size=(25, 15),
         herbivores_start_count=1,
         predators_start_count=0,
-        grass_count=0,
+        grass_count=5,
         rock_count=0,
         predator_search_algoritm="bfs",
         herbivore_search_algoritm="bfs",
@@ -18,15 +18,14 @@ if __name__ == "__main__":
         rock_pict="images/Obstacle.png",
         predator_pict="images/Predator.png",
         herbivore_pict="images/Herbivore.png",
+        ground_pict="images/Ground.png",
         grass_pict="images/Grass.png",
-        grid_color = "gray",
+        grid_color="gray",
     )
 
-   
     b = Backend(backend_config)
     f = GuiFrontend(gui_config)
     f.use_backend(b)
-
 
     f.window_init()
     f.run()
