87055861d788c0db589aee2d73fd102beb564c19
Alexey Melnikov <melnikov1991@bk.ru>
2025-02-27 16:14:50 +0300

    fix: remove map_init from simulation

--- src/simulation/simulation.py
+++ src/simulation/simulation.py
@@ -5,7 +5,6 @@ class Simulation:
         self._simulation_init()
 
     def _simulation_init(self):
-        self.backend.map_init()
         self.frontend.use_backend(self.backend)
 
     def run(self):
