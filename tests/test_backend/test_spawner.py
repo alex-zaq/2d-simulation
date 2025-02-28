15a2a034a8b8b350378c614c56b856e8bf4db159
alex <melnikov1991@bk.ru>
2025-02-28 23:49:56 +0300

    test: update test_spawner

--- tests/test_backend/test_spawner.py
+++ tests/test_backend/test_spawner.py
@@ -5,7 +5,7 @@ def test_coord_generator():
     
     coord_generator = CoordGenerator(100, 100)
     
-    res = coord_generator.get_non_repeating_coords_series(30)
+    res = coord_generator.get_non_rpt_coords_srs(30)
 
     assert len(res) == 30
 
