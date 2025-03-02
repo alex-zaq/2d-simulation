9c58c67250b4bda7862a5b928a891cc1b930304e
alex <melnikov1991@bk.ru>
2025-03-02 21:58:38 +0300

    test: rename test_spawner

--- tests/test_frontend/test_gui_frontend.py
+++
@@ -1,5 +0,0 @@
-from simulation.frontend.gui_frontend import GuiFrontend
-
-
-def test_zaq():
-    assert 1 == 1
--- tests/test_spawner/test_spawner.py
+++ tests/test_spawner/test_spawner.py
@@ -2,13 +2,7 @@ from simulation.backend.spawner import CoordGenerator
 
 
 def test_coord_generator():
-    
     coord_generator = CoordGenerator(100, 100)
-    
     res = coord_generator.get_non_rpt_coords_srs(30)
-
+    res = set(res)
     assert len(res) == 30
-
-     
-
-
