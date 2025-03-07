7517dcb52816939263ea7d405510d94027f0c1eb
alex <melnikov1991@bk.ru>
2025-03-07 19:16:52 +0300

    feat: add A-star searching algorithm

--- src/simulation/backend/behavior_algoritms/searching_algoritms.py
+++ src/simulation/backend/behavior_algoritms/searching_algoritms.py
@@ -1,3 +1,4 @@
+import heapq
 from collections import deque
 from enum import Enum, auto
 
@@ -26,10 +27,14 @@ class Search_base:
                 return Search_result(path[1], Creature_status.EATING)
             return Search_result(path[1], Creature_status.SEARCHING)
         else:
-            random_coords = game_map.get_closest_random_valid_coords(
-                (entity.x, entity.y), entity, target_cls
-            )
-            return Search_result(random_coords, Creature_status.WANDERING)
+            return cls._get_random_move_results(game_map, entity, target_cls)
+
+    @classmethod
+    def _get_random_move_results(cls, game_map, entity, target_cls):
+        random_coords = game_map.get_closest_random_valid_coords(
+            (entity.x, entity.y), entity, target_cls
+        )
+        return Search_result(random_coords, Creature_status.WANDERING)
 
 
 class BFS(Search_base):
@@ -66,50 +71,88 @@ class BFS(Search_base):
 
 
 class A_star(Search_base):
+    class Node:
+        def __init__(self, coords, g=0, h=0):
+            self.coords = coords
+            self.g = g
+            self.h = h
+
+        def __lt__(self, other):
+            return self.g + self.h < other.g + other.h
+
+        def __eq__(self, other):
+            return self.coords == other.coords
+
     @classmethod
-    def search(cls, game_map, entity, target):
-        pass
+    def get_heuristic_func(cls, game_map, entity):
+        match type(entity).__name__:
+            case "Predator":
+                if game_map.config.predator_through_wall:
+                    return game_map.distance_through_walls
+                else:
+                    return game_map.distance
+            case "Herbivore":
+                if game_map.config.herbivore_through_wall:
+                    return game_map.distance_through_walls
+                else:
+                    return game_map.distance
 
-    # @classmethod
-    # def _path_processor(cls, entity, path, game_map, target_cls):
-    #     if path:
-    #         if len(path) == 2:
-    #             return cls.Search_result(path[1], Creature_status.EATING)
-    #         return cls.Search_result(path[1], Creature_status.SEARCHING)
-    #     else:
-    #         random_coords = cls._get_closest_random_valid_coords((entity.x, entity.y), game_map, target_cls)
-    #         return cls.Search_result(random_coords, Creature_status.WANDERING)
+    @classmethod
+    def search(cls, game_map, entity, target_cls):
+        x, y = entity.get_coords()
+        goal = game_map.get_closest_target_by_coords((x, y), target_cls)
 
-    # @classmethod
-    # def _get_neighbors(cls, game_map, x_y_coords, target_cls):
-    #     max_x, max_y = game_map.config.map_size
-    #     max_x -= 1
-    #     max_y -= 1
-    #     x, y = x_y_coords
-    #     neighbors = []
-    #     for dx in [-1, 0, 1]:
-    #         for dy in [-1, 0, 1]:
-    #             if dx == 0 and dy == 0:
-    #                 continue
-    #             nx, ny = x + dx, y + dy
-    #             if nx < 0 or ny < 0 or nx > max_x or ny > max_y:
-    #                 continue
-    #             if cls._is_target(game_map, (nx, ny), target_cls):
-    #                 neighbors.append((nx, ny))
-    #             if (nx, ny) not in game_map.map:
-    #                 neighbors.append((nx, ny))
-    #     return neighbors
+        if goal is None:
+            return cls._get_random_move_results(game_map, entity, target_cls)
 
-    # @classmethod
-    # def _is_target(cls, game_map, check_x_y_coords, target_cls):
-    #     if check_x_y_coords not in game_map.map:
-    #         return False
-    #     return isinstance(game_map.map[check_x_y_coords], target_cls)
+        goal_coords = goal.get_coords()
 
-    # @classmethod
-    # def _get_closest_random_valid_coords(cls, coord_pair, game_map, target_cls):
-    #     coords = cls._get_neighbors(game_map, coord_pair, target_cls)
-    #     if coords:
-    #         return random.choice(coords)
-    #     else:
-    #         return coord_pair
+        open_list = []
+        closed_set = set()
+        parents = {}
+        heuristic_func = cls.get_heuristic_func(game_map, entity)
+        path = []
+
+        heapq.heappush(open_list, cls.Node((x, y)))
+
+        while open_list:
+            current_node = heapq.heappop(open_list)
+            curr_coords = current_node.coords
+
+            if game_map.is_target(curr_coords, target_cls):
+                finish_coords = curr_coords
+                while curr_coords in parents:
+                    curr_coords = parents[curr_coords]
+                    path.append(curr_coords)
+                path = path[::-1]
+                path.append(finish_coords)
+                return cls._path_processor(entity, path, game_map, target_cls)
+
+            closed_set.add(curr_coords)
+
+            neighbors = game_map.get_neighbors(curr_coords, entity, target_cls)
+
+            new_g = current_node.g + 1
+
+            for n_coords in neighbors:
+                if n_coords not in closed_set:
+                    if node_to_update := next(
+                        (n for n in open_list if n.coords == n_coords), None
+                    ):
+                        if new_g < current_node.g:
+                            node_to_update.g = new_g
+                            node_to_update.h = heuristic_func(
+                                node_to_update.coords, goal_coords
+                            )
+                            parents[node_to_update.coords] = current_node.coords
+                            heapq.heapify(open_list)
+                    else:
+                        new_node = cls.Node(
+                            n_coords,
+                            g=new_g,
+                            h=heuristic_func(curr_coords, goal_coords),
+                        )
+                        heapq.heappush(open_list, new_node)
+                        parents[n_coords] = current_node.coords
+
+        return cls._path_processor(entity, path, game_map, target_cls)
